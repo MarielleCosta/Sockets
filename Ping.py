c979278da8be685067d903bcae9977f248650490
Marielle Cristina <98425117+MarielleCosta@users.noreply.github.com>
2023-08-31 09:47:02 -0300

    Atualização
    Organização do código

--- Ping.py
+++ Ping.py
@@ -1,38 +1,40 @@
 import socket
 import sys
 
-##---------------------------------Socket UDP---------------------------------##
-
-UDP_HOST = "127.0.0.1" #sys
-UDP_PORT = 20001 #sys pong
-
-#Cria um socket datagrama
-UDP_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#UDP_sock.bind((UDP_HOST, UDP_PORT))
-print("Servidor UDP escutando")
-
-##---------------------------------Socket TCP---------------------------------##
+# Recebe parâmetros
+UDP_HOST = sys.argv[1] #Endereço Pong
+UDP_PORT = sys.argv[2] #Porto Pong
 
 TCP_HOST = "localhost"
-TCP_PORT = 5000 #sys port pong
+TCP_PORT = sys.argv[3] #Porto Ping
 
+# Cria um socket datagrama UDP
+UDP_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#UDP_sock.bind((UDP_HOST, UDP_PORT))
+
+# Cria um socket TCP 
 TCP_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # (TCP = SOCK_STREAM/UDP = SOCK_DGRAM)
-TCP_sock.bind((TCP_HOST, TCP_PORT)) ###Liga o socket TCP ao porto_ping
-TCP_sock.listen() ###Prepara o socket para receber conexões
+
+# Liga o socket TCP ao porto_ping
+TCP_sock.bind((TCP_HOST, TCP_PORT))
+# Prepara o socket para receber conexões
+TCP_sock.listen()
 print("Aguardando conexão do Ping")
 
-#####Verificar como montar a mensagem de ping, é de qual endereço Ping ou Pong?
-IP_local =socket.gethostbyname(socket.gethostname())
-msg_de_ping = str(IP_local) + ':' + str(UDP_PORT)
+# Monta mensagem de ping
+IP_local_Ping =socket.gethostbyname(socket.gethostname())
+msg_de_ping = str(IP_local_Ping) + ':' + str(TCP_PORT)
 
+# Envia mensagem de ping
 UDP_sock.sendto(str.encode(msg_de_ping), UDP_HOST)
 
-conn,endereco = TCP_sock.accept() #Retorna o endereço que está conectado
+# Prepara para aceitar a conexão TCP vinda do Pong
+conn,endereco = TCP_sock.accept()
 
+# Espera pela mensagem de Pong 
 while True:
-    data = conn.recv(4096) #Recebe 1024 bytes
-    #Extrai mensagem recebida
-    ## Mensagem recebida: IP:porta do ping
+    data = conn.recv(4096) #Recebe mensagem
+    # Extrai mensagem recebida
     print(data)
     print("Tamanho mensagem: ", len(data))
     if not data: #Verifica se acabou de receber os dados
--- Pong.py
+++ Pong.py
@@ -1,21 +1,24 @@
 import socket
 import sys
 
-##---------------------------------Socket UDP---------------------------------##
-
+# Recebe parâmetros 
 UDP_HOST   = "127.0.0.1"
-UDP_PORT = 20001 #sys
-mensagem_entrada ="OLa mundo" #sys
+UDP_PORT = sys.argv[1]  #Porto Pong
+msg_de_pong = sys.argv[2]   #String Pong
 
+# Cria um socket UDP
 UDP_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Liga o socket ao porto Pong
 UDP_sock.bind((UDP_HOST, UDP_PORT))
 
+# Espera uma mensgaem msg_de_ping
 while(True):
     bytesAddressPair = UDP_sock.recvfrom(4096)
     mensagem = bytesAddressPair[0]
     endereco = bytesAddressPair[1]
 
-    #Extrai mensagem recebida
+    # Extrai mensagem recebida
     ## Mensagem recebida: IP:porta do ping
     print(mensagem)
     mensagem_separada = mensagem.decode("UTF-8").split(":")
@@ -26,15 +29,19 @@ while(True):
     
     break
 
-##---------------------------------Socket TCP---------------------------------##
-
+# Associa o IP e a porta Ping
 TCP_HOST = IP_Ping
 TCP_PORT = Port_Ping
 
-TCP_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket
-TCP_sock.connect((TCP_HOST,TCP_PORT)) #conecta o socket no servidor
-TCP_sock.sendall(str.encode(mensagem_entrada)) #Envia mensagem
+# Cria um socket TCP
+TCP_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-TCP_sock.close()
+# Conecta o socket TCP no Ping
+TCP_sock.connect((TCP_HOST,TCP_PORT)) 
 
+# Envia mensagem msg_de_pong
+TCP_sock.sendall(str.encode(msg_de_pong))
+
+# Fecha as conexões
 UDP_sock.close()
+TCP_sock.close()
